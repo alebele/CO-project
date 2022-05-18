53e22dc18e1c678a634d90002f76e6885888448a
beatriceToader <acribiebea@gmail.com>
2022-05-18 14:55:09 +0300

    behind

--- src/main/java/com/example/co_project/Controller.java
+++ src/main/java/com/example/co_project/Controller.java
@@ -1,13 +1,17 @@
 package com.example.co_project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,6 +30,9 @@ public class Controller implements Initializable{
     @FXML
     private ImageView myImageView;
 
+    @FXML
+    private AnchorPane scene_01;
+
     @Override
     public void initialize(URL URL, ResourceBundle resourceBundle){
         File backgroundFile = new File("src/Images/MonalisaImage.jpg");
@@ -34,4 +41,10 @@ public class Controller implements Initializable{
         myImageView.setImage(myImage);
     }
 
+    @FXML
+    private void openSecondScene(ActionEvent event) throws IOException {
+        AnchorPane scene_02= FXMLLoader.load(getClass().getResource("SecondScene.fxml"));
+        scene_01.getChildren().removeAll();
+        scene_01.getChildren().setAll(scene_02);
+    }
 }
